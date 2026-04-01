f30a55ab6549d4d92958800847e92a143c99bdf8
katherin ross <wvalersanchez@gmail.com>
2026-04-01 12:06:27 -0500

    Inicio

---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/app.css">
+    <title>Gourmet Criollo</title>
+</head>
+<body>
+    <div class="layout">
+        <header class="header">
+            <div class="header__contenido container">
+                <p class="header__logo">Gourmet Criollo</p>
+                <nav class="header__nav">
+                    <ul>
+                        <li><a href="#">Inicio</a></li>
+                        <li><a href="#">Carta</a></li>
+                        <li><a href="#">Blog</a></li>
+                        <li><a href="#">Contacto</a></li>
+                    </ul>
+                </nav>
+                <a class="cta-primary" href="#">Reservar Mesa</a>
+            </div>
+        </header>
+        <main>
+            <!--HERO SECTION-->
+            <section class="hero">
+                <div class="hero__overlay">
+                    <div class="hero__contenido container">
+                        <h2>Gourmet Criollo</h2>
+                        <p>Sabor que honra la tradición</p>
+                        <a class="cta-primary" href="#">Reservar mesa</a>
+                    </div>
+                </div>
+            </section>
+
+
+  <section class="menu container">
+    <div class="menu__carta">
+        <h2>Nuestra carta</h2>
+        <div class="carta__contenido">
+            
+            <div class="carta__seccion">
+                <h3>Entrada</h3>
+                <div class="carta__item">
+                    <p>Causa Limeña</p>
+                    <p>S/. 20.00</p>
+                </div>
+                <div class="carta__item">
+                    <p>Papa a la huancaína</p>
+                    <p>S/. 18.00</p>
+                </div>
+            </div>
+
+            <div class="carta__seccion">
+                <h3>Fondo</h3>
+                <div class="carta__item">
+                    <p>Tallarín saltado</p>
+                    <p>S/. 20.00</p>
+                </div>
+                <div class="carta__item">
+                    <p>Seco de cordero con frejoles</p>
+                    <p>S/. 35.00</p>
+                </div>
+            </div>
+
+            <div class="carta__seccion">
+                <h3>Postre</h3>
+                <div class="carta__item">
+                    <p>Mazamorra morada</p>
+                    <p>S/. 10.00</p>
+                </div>
+                <div class="carta__item">
+                    <p>Mazamorra de calabaza</p>
+                    <p>S/. 10.00</p>
+                </div>
+                <div class="carta__item">
+                    <p>Arroz con leche</p>
+                    <p>S/. 10.00</p>
+                </div>
+            </div>
+
+        </div> </div> </section>
+        </main>
+        <footer class="footer">
+            <p>Footer</p>
+        </footer>
+    </div>
+</body>
+</html>
